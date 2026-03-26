9baf3b38b29813529dc3a04c748caabe00aa4cd0
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-26 22:28:14 +0300

    styles: write inline styles for blocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useQuery} from "@apollo/client/react";
+import {GET_ALL_USERS} from "./query/user.js";
 
 function App() {
+    const {data, loading, error} = useQuery(GET_ALL_USERS)
   const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+
+    }, [data]);
   return (
     <div>
-      <form>
+      <form style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px',
+          alignItems: 'center'
+      }}>
         <input type="text"/>
         <input type="number"/>
-        <div className="btns">
+        <div style={{display: 'flex', gap: '10px'}}>
           <button>Создать</button>
           <button>Получить</button>
         </div>
       </form>
       <div>
         {users.map(user =>
-            <div className="user">{user.id}. {user.username} {user.age}</div>
+            <p className="user">{user.id}. {user.username} {user.age}</p>
         )}
       </div>
     </div>
