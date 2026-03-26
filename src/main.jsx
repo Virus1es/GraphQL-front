bb8da14c25c549c13016df7b815e12fe0eb4a6e6
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-26 22:11:41 +0300

    feat: use apollo

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
 
-createRoot(document.getElementById('root')).render(<App/>,)
+const client = new ApolloClient({
+    uri: 'http://localhost:5000/graphql',
+    cache: new InMemoryCache()
+})
+
+createRoot(document.getElementById('root')).render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+)
