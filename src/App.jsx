f273b0ef5407d5f1b313abb3c6d4281d5a450185
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-26 22:39:31 +0300

    feat: use graphql query to get all users

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,40 @@ import {GET_ALL_USERS} from "./query/user.js";
 
 function App() {
     const {data, loading, error} = useQuery(GET_ALL_USERS)
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
+        if(!loading) {
+            setUsers(data.getAllUsers);
+        }
+    }, [data, loading]);
 
-    }, [data]);
-  return (
-    <div>
-      <form style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '10px',
-          alignItems: 'center'
-      }}>
-        <input type="text"/>
-        <input type="number"/>
-        <div style={{display: 'flex', gap: '10px'}}>
-          <button>Создать</button>
-          <button>Получить</button>
+    if(loading) {
+        return <h1>Loading...</h1>;
+    }
+
+    return (
+        <div>
+            <form style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '10px',
+                alignItems: 'center'
+            }}>
+                <input type="text"/>
+                <input type="number"/>
+                <div style={{display: 'flex', gap: '10px'}}>
+                <button>Создать</button>
+                <button>Получить</button>
+                </div>
+            </form>
+            <div>
+                {users.map(user =>
+                    <p className="user">{user.id}. {user.username} {user.age}</p>
+                )}
+            </div>
         </div>
-      </form>
-      <div>
-        {users.map(user =>
-            <p className="user">{user.id}. {user.username} {user.age}</p>
-        )}
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
